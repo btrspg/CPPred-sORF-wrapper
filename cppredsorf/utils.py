0939ed944adefa13558c68d7cfd5092e9d724e39
logan <yuelong.chen.btr@gmail.com>
2020-08-31 16:48:16 +0800

    [M]: add env

--- cppredsorf/utils.py
+++ cppredsorf/utils.py
@@ -7,5 +7,5 @@ def get_model_range_hexamer():
 
     m_f = pkg_resources.resource_filename(__name__, 'Model/Integrated.model')
     r_f = pkg_resources.resource_filename(__name__, 'Model/Integrated.range')
-    h_f = pkg_resources.resource_filename(__name__, 'Model/Intwgrated_Hexamer.tsv')
+    h_f = pkg_resources.resource_filename(__name__, 'Model/Integrated_Hexamer.tsv')
     return m_f, r_f, h_f
